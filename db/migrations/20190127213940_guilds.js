cef848f80170d47554bc0a3c31235520d7fecf19
Vikanda Gonzales <vikanda.gonzales@yahoo.com>
2019-03-10 15:22:15 -0700

    added default values to guilds table in db

--- db/migrations/20190127213940_guilds.js
+++ db/migrations/20190127213940_guilds.js
@@ -4,8 +4,8 @@ exports.up = (knex, Promise) => {
   return knex.schema.createTable(TABLE_NAME, table => {
     table.increments();
     table.string('name').notNullable();
-    table.string('status').notNullable();
-    table.timestamp('founded').notNullable();
+    table.string('status').defaultsTo(null);
+    table.timestamp('founded').defaultsTo(null);
     table.string('logo').defaultsTo(null);
     table.boolean('archived').defaultsTo(false);
     table.timestamp('created_at').defaultTo(knex.fn.now());
